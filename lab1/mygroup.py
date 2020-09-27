f35cc7ace2bd40cb812ff7ded54876e5930eb92e
hjarnu <60552856+hjarnu@users.noreply.github.com>
2020-09-27 16:23:49 +0300

    123

--- lab1/mygroup.py
+++ lab1/mygroup.py
@@ -31,7 +31,7 @@ groupmates = [
 "marks": [5, 5, 5]
 }
 ]
-def print_students(students):
+def filter(students):
     print(u"Имя".ljust(10), u"Фамилия".ljust(10), u"Экзамены".ljust(40), u"Оценки".ljust(10))
     for student in students:
             mark = student['marks']
@@ -39,4 +39,4 @@ def print_students(students):
             if avg >= x:
                 print(student["name"].ljust(10), student["surname"].ljust(10), str(student["exams"]).ljust(40), str(student["marks"]).ljust(10))
 x = float(input('Введите средний балл для фильтрации: '))
-print_students(groupmates);
+filter(groupmates);
